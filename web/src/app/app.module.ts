775d002165ac04ec353b2ba6cd37602e143ba32a
Jan Navarro <navarrojanderrick@gmail.com>
2022-03-27 10:18:35 -0700

    Init ngx-bootstrap module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,13 @@ import { ApiModule } from './api/api.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FirebaseModule, ApiModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FirebaseModule,
+    ApiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ web/src/app/ngx-bootstrap/ngx-bootstrap.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, ButtonsModule.forRoot()],
+})
+export class NgxBootstrapModule {}
